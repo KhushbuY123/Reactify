ffb1931369c6db9ddab30a5a7335286038f4d3b0
Khushbu Yadav <khushbuyad123@gmail.com>
2024-04-04 23:33:17 +0530

    Day 2

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-const heading=React.createElement("h2",{
-    id:"heading",axy:"abc"
-},"Hello World in  React Without Using React ") //This is just and object
-// Create nested react structure
+import React from "react"
+import  ReactDOM  from "react-dom/client"
 const parent=React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"Hi I am h1 tag"),
+        React.createElement("h1",{},"Hi I am h1 tag 🚀"),
         React.createElement("h2",{},"I am h2 tag")
     ]),
     React.createElement("div",{id:"child2"},[
         React.createElement("h1",{},"This is another h1"),
         React.createElement("h2",{},"This is another h2")
-    ])
-])
+    ]),
+]);
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent) //Take the object and put it over there
+root.render(parent)
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # React Learning 🚀
+---
+
+# Parcel
+-Dev Build
+-Local Builder
+-HMR = Hot Module Replacement
+-File Watching Algorithm - Written in C++
+-Caching - Faster Builds
+-Image Optimization
+-Minfication
+-Bundling
+-Compress
+-Consistent Hashing
+-Code Splitting 
+-Diffrential Bundling - support older browser
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code 
+-Different dev and prod bundles
+
